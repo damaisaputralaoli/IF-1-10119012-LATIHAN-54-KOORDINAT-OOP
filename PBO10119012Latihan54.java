7b7e81a5c80deed060499f4853a3c0ddd24f86ed
damaisaputralaoli <73173498+damaisaputralaoli@users.noreply.github.com>
2020-11-22 10:38:11 +0700

    Add files via upload

---
+++ PBO10119012Latihan54.java
@@ -0,0 +1,19 @@
+
+package pbo10119012latihan54;
+
+/**
+ *  NAMA  : DAMAI SAPUTRA LAOLI
+ *  KELAS : IF-1
+ *  NIM   : 10119012
+ * @author damai
+ */
+public class PBO10119012Latihan54 {
+
+    
+    public static void main(String[] args) {
+        WarnaKoordinat loc = new WarnaKoordinat(10,4,"Jingga");
+
+        System.out.println("Warna koordinat   : " + loc.getNamaWarna());
+        System.out.println("Koordinat sumbu x : " + loc.getX() + ", y : " + loc.getY());
+    }
+}
